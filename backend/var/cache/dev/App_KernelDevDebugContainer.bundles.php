908e2ef0d2a5f9b1698535773e3f761f5f59b59c
Yuliy Futimsky <yulikfwork@gmail.com>
2026-07-13 13:08:58 +0200

    init

---
+++ backend/var/cache/dev/App_KernelDevDebugContainer.bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'ServicesBundle' => new \Symfony\Component\DependencyInjection\Kernel\ServicesBundle(),
+    'ConsoleBundle' => new \Symfony\Component\Console\ConsoleBundle(),
+    'FrameworkBundle' => new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+];
