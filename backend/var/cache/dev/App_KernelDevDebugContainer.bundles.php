55bc36c38607dab4af4edb5037ad25ccc2dd8bf4
Yuliy Futimsky <yulikfwork@gmail.com>
2026-07-13 14:47:31 +0200

    add docker

--- backend/var/cache/dev/App_KernelDevDebugContainer.bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'ServicesBundle' => new \Symfony\Component\DependencyInjection\Kernel\ServicesBundle(),
-    'ConsoleBundle' => new \Symfony\Component\Console\ConsoleBundle(),
-    'FrameworkBundle' => new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-];
